54237033bdd43728e5afc7483fefd954d54cc5db
ncaq <ncaq@ncaq.net>
2013-07-16 01:11:38 +0900

    整理

--- update.zsh
+++ update.zsh
@@ -3,13 +3,11 @@ update ()
     sudo emerge --sync
     sudo layman -S
     sudo eix-update
-#sudo emerge -uDN world
-
     sudo gem update
     sudo cabal update
 
-    sudo updatedb
     source /home/ncaq/.oh-my-zsh/custom/gitallpull.zsh
     gitallpull
+
     sudo updatedb
 }
