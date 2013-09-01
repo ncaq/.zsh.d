076a4adbb168324ee08a1134474d0bfe470dc2c0
ncaq <ncaq@ncaq.net>
2013-09-02 02:10:51 +0900

    htns

--- bindkey.zsh
+++ bindkey.zsh
@@ -1,4 +1,10 @@
 bindkey -s '\el' "l\n"
 
-bindkey "^[h" backward-kill-word
-bindkey "^[^H" run-help
+bindkey "^s" forward-char
+bindkey "^h" backward-char
+bindkey "^b" backward-delete-char
+bindkey "^t" history-beginning-search-backward-end
+bindkey "^[h" backward-word
+bindkey "^[s" forward-word
+bindkey "^[b" backward-kill-word
+bindkey "^[t" history-search-backward
