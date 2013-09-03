51a7bf7b7a08f2176d66cb3296cf467f3f151ba8
ncaq <ncaq@ncaq.net>
2013-09-03 23:36:44 +0900

    diff

--- path.zsh
+++ path.zsh
@@ -1,5 +1,21 @@
-PATH=/usr/lib/ccache/bin:/usr/lib/distcc/bin:/sbin/:/usr/sbin/:/usr/bin:/usr/games/bin:/opt/copy/:~/.cabal/bin/:~/Documents/tool:${PATH}
+PATH="
+/usr/lib/ccache/bin:
+/usr/lib/distcc/bin:
+/sbin/:
+/usr/sbin/:
+/usr/bin:
+/usr/games/bin:
+/opt/copy/:
+~/.cabal/bin/:
+~/Documents/tool:
+${PATH}
+"
 export PATH
 
-LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/lib:/usr/lib/:/usr/local/lib
+LD_LIBRARY_PATH="
+/lib:
+/usr/lib/:
+/usr/local/lib:
+${LD_LIBRARY_PATH}
+"
 export LD_LIBRARY_PATH
