ebf030e7fcbb19dd06b634b50d5f9836f1a2fd96
ncaq <ncaq@ncaq.net>
2013-09-03 22:53:45 +0900

    path

--- path.zsh
+++ path.zsh
@@ -1,4 +1,4 @@
-PATH=${PATH}:/sbin/:/usr/sbin/:/usr/bin:/usr/games/bin:/opt/copy/:~/.cabal/bin/:~/Documents/tool
+PATH=${PATH}:/sbin/:/usr/sbin/:/usr/bin:/usr/lib/ccache/bin:/usr/lib/distcc/bin:/usr/games/bin:/opt/copy/:~/.cabal/bin/:~/Documents/tool
 export PATH
 
 LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/lib:/usr/lib/:/usr/local/lib
