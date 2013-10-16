4e04e3db3478564a4d4e3cff5da1d528c279ed08
ncaq <ncaq@ncaq.net>
2013-10-17 01:29:04 +0900

    history complete delete

--- completion.zsh
+++ completion.zsh
@@ -31,7 +31,7 @@ setopt glob_dots
 zstyle ':completion:*:default' menu select=2
 # 補完関数の表示を強化する
 zstyle ':completion:*' verbose yes
-zstyle ':completion:*' completer _expand _complete _match _prefix _approximate _list _history
+zstyle ':completion:*' completer _expand _complete _match _prefix _approximate _list
 zstyle ':completion:*:messages' format '%F{YELLOW}%d'$DEFAULT
 zstyle ':completion:*:warnings' format '%F{RED}No matches for:''%F{YELLOW} %d'$DEFAULT
 zstyle ':completion:*:descriptions' format '%F{YELLOW}completing %B%d%b'$DEFAULT
