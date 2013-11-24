6b2bdcc64e2b5cea4c994ec3f3ba6a3f3656359f
ncaq <ncaq@ncaq.net>
2013-11-25 01:03:52 +0900

    add path pkg-config

--- path.zsh
+++ path.zsh
@@ -1,5 +1,5 @@
-PATH="/sbin/:/usr/sbin/:/usr/bin:/usr/games/bin:/home/ncaq/.cabal/bin/:/home/ncaq/Documents/tool:${PATH}"
-export PATH
+export PATH="/sbin/:/usr/sbin/:/usr/bin:/usr/games/bin:/home/ncaq/.cabal/bin/:/home/ncaq/Documents/tool:${PATH}"
 
-LD_LIBRARY_PATH="/lib:/usr/lib/:/usr/local/lib:${LD_LIBRARY_PATH}"
-export LD_LIBRARY_PATH
+export LD_LIBRARY_PATH="/lib:/usr/lib/:/usr/lib64/:/usr/local/lib:${LD_LIBRARY_PATH}"
+
+export PKG_CONFIG_PATH="/usr/local/lib64/pkgconfig/"
