6386e228ee9aa9f67cdd6660c1b839a9793a6eb2
ncaq <ncaq@ncaq.net>
2013-12-31 23:52:39 +0900

    マウントも自動でするように

--- alias.zsh
+++ alias.zsh
@@ -3,7 +3,7 @@ alias mozc-config='/usr/lib/mozc/mozc_tool -mode=config_dialog'
 alias mozc-dict='/usr/lib/mozc/mozc_tool --mode=dictionary_tool'
 #grub手動で書き込むの面倒
 alias grub-update='sudo grub2-mkconfig -o /boot/grub/grub.cfg'
-alias grub-install='sudo grub2-install --target=x86_64-efi'
+alias grub-install='sudo mount /boot/efi && sudo grub2-install --target=x86_64-efi'
 #get full path
 alias fullpath='find `pwd` -maxdepth 1 -mindepth 1'
 #mouse mode 0=disable 1=enable
