0a331eae9e29f381e8fb0deb8c5e2b4f05426083
ncaq <ncaq@ncaq.net>
2014-01-08 17:00:26 +0900

    名前変更と使ってないの消去

--- extension.zsh
+++
@@ -1,7 +0,0 @@
-#alias -s
-alias -s hs=ghci
-alias -s html=firefox
-alias -s py=python
-alias -s lzh="lha e"
-alias -s {gz,tgz,zip,bz2,tbz,Z,tar,arj,xz}=x
-alias -s {png,jpg,bmp,PNG,JPG,BMP}=xdg-open
