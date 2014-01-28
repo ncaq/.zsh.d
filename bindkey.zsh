d1a6134eacc885a8fda9631d512387a30b44cb57
ncaq <ncaq@ncaq.net>
2014-01-28 19:01:12 +0900

    emacs向けに調整してみたけどよくわかんない

--- bindkey.zsh
+++ bindkey.zsh
@@ -1,11 +1,16 @@
 bindkey -s '\el' "l\n"
 
-bindkey "^[b" backward-kill-word
-bindkey "^[h" backward-word
-bindkey "^[s" forward-word
-bindkey "^[t" history-search-backward
-bindkey "^b" backward-delete-char
-bindkey "^h" backward-char
-bindkey "^n" history-beginning-search-forward-end
-bindkey "^s" forward-char
-bindkey "^t" history-beginning-search-backward-end
+if [ "xterm" = $TERM ]
+then
+    bindkey "^[b" backward-kill-word
+    bindkey "^[h" backward-word
+    bindkey "^[s" forward-word
+    bindkey "^[t" history-search-backward
+    bindkey "^b" backward-delete-char
+    bindkey "^h" backward-char
+    bindkey "^n" history-beginning-search-forward-end
+    bindkey "^s" forward-char
+    bindkey "^t" history-beginning-search-backward-end
+else
+    bindkey "^[h" backward-word
+fi
