e6e92635ef742f041d1ccd990ce8351b3e68f24c
ncaq <ncaq@ncaq.net>
2014-03-10 01:08:00 +0900

    fix

--- profile.d/load.zsh
+++ profile.d/load.zsh
@@ -1,6 +1,9 @@
+source /etc/profile.d/autojump.zsh
+
 autoload history-search-end
 autoload percol-locate-cd
 autoload percol-select-history
+
 zle -N history-beginning-search-backward-end history-search-end
 zle -N history-beginning-search-forward-end history-search-end
 zle -N percol-select-history
