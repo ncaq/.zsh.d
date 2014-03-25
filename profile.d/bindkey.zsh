1fb361da8918ddc60ec3a0b09f762b6cc13baa3b
ncaq <ncaq@ncaq.net>
2014-03-25 22:25:21 +0900

    sudo -E option is break in xmonad.

--- profile.d/bindkey.zsh
+++ profile.d/bindkey.zsh
@@ -1,20 +1,15 @@
+stty stop undef
+
 bindkey -s '\el' "l\n"
 
 bindkey "^[[Z" reverse-menu-complete #Shift-Tabで補完候補を逆順する("\e[Z"でも動作する) http://d.hatena.ne.jp/oovu70/20120405/p1
+bindkey "^[b" backward-kill-word
+bindkey "^[h" backward-word
+bindkey "^[s" forward-word
+bindkey "^[t" history-search-backward
+bindkey "^b" backward-delete-char
+bindkey "^h" backward-char
+bindkey "^n" history-beginning-search-forward-end
+bindkey "^s" forward-char
+bindkey "^t" history-beginning-search-backward-end
 bindkey '^r' percol-select-history
-
-#ansi-term対策
-if [ -z $EMACS ]
-then
-    bindkey "^[b" backward-kill-word
-    bindkey "^[h" backward-word
-    bindkey "^[s" forward-word
-    bindkey "^[t" history-search-backward
-    bindkey "^b" backward-delete-char
-    bindkey "^h" backward-char
-    bindkey "^n" history-beginning-search-forward-end
-    bindkey "^s" forward-char
-    bindkey "^t" history-beginning-search-backward-end
-else
-    bindkey "^[h" backward-word
-fi
