a90c4e0280e8d4d39bbefe8784b8c8a9c9b984c4
ncaq <ncaq@ncaq.net>
2014-04-09 17:27:16 +0900

    fix

--- profile.d/bindkey.zsh
+++ profile.d/bindkey.zsh
@@ -13,4 +13,4 @@ bindkey "^n" history-beginning-search-forward-end
 bindkey "^s" forward-char
 bindkey "^t" history-beginning-search-backward-end
 bindkey '^r' percol-history
-bindkey '^u' percol-cd
+bindkey '^f' percol-cd
