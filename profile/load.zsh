13c0e588f7182340886dc79be68fae2e2dab6afb
ncaq <ncaq@ncaq.net>
2014-05-25 18:47:19 +0900

    file exist check

--- profile/load.zsh
+++ profile/load.zsh
@@ -1,12 +1,14 @@
 setopt autopushd
 
-source /etc/profile.d/autojump.zsh
-
 autoload history-search-end
-autoload percol-cd
-autoload percol-history
-
 zle -N history-beginning-search-backward-end history-search-end
 zle -N history-beginning-search-forward-end history-search-end
+
+autoload percol-cd
+autoload percol-history
 zle -N percol-history
 zle -N percol-cd
+
+if [ -f "/etc/profile.d/autojump.zsh" ];then
+source /etc/profile.d/autojump.zsh
+fi
