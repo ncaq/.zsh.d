dd038740017432ef1f2bf2dc6ee2fdd7fe50672f
ncaq <ncaq@ncaq.net>
2014-08-24 23:02:22 +0900

    change PROMPT

--- profile/prompt.zsh
+++ profile/prompt.zsh
@@ -1,5 +1,4 @@
-PROMPT="%B%U%F{green}%n@%m%f %(?,%F{white},%F{red})%?%f %F{blue}%y%f %~
-%#%b%u "
+PROMPT="%K{magenta}%B%U%F{green}%n@%m%f %(?,%F{white},%F{red})%?%f %F{blue}%y%f %~%b%u%k%#"
 
 #ls when cd
 function chpwd() {ls}
