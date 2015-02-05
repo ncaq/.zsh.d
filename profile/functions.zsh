ea24b93ca055cdec37953d54ef0ca85f2aa75b3d
ncaq <ncaq@ncaq.net>
2015-02-05 22:34:49 +0900

    functions.zsh -> autoload

--- profile/functions.zsh
+++
@@ -1,9 +0,0 @@
-locate-current-dir()
-{
-    locate --ignore-case --regex "^$(pwd).*$@.*"
-}
-
-locate-current-dir-null()
-{
-    locate --null --ignore-case --regex "^$(pwd).*$@.*"
-}
--- profile/load.zsh
+++ profile/load.zsh
@@ -1,5 +1,13 @@
 setopt autopushd
 
+if [ -f "$ZDOTDIR/sub/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" ];then
+    source $ZDOTDIR/sub/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+fi
+
+if [ -f "/etc/profile.d/autojump.zsh" ];then
+    source /etc/profile.d/autojump.zsh
+fi
+
 autoload history-search-end
 zle -N history-beginning-search-backward-end history-search-end
 zle -N history-beginning-search-forward-end history-search-end
@@ -12,10 +20,5 @@ zle -N percol-subfind
 
 autoload pr
 
-if [ -f "$ZDOTDIR/sub/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" ];then
-    source $ZDOTDIR/sub/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-fi
-
-if [ -f "/etc/profile.d/autojump.zsh" ];then
-    source /etc/profile.d/autojump.zsh
-fi
+autoload locate-current-dir
+autoload locate-current-dir-null
