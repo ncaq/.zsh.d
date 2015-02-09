3ad9e990f00939bc6f02304a2f733d2eefe3c9fb
ncaq <ncaq@ncaq.net>
2015-02-10 01:08:35 +0900

    alias: partition file

---
+++ profile/alias_ls.zsh
@@ -0,0 +1,3 @@
+alias ls='ls-first-ln-dir --color=auto --classify --human-readable --almost-all'
+alias ll='ls-first-ln-dir --color=auto --classify --human-readable --all -l'
+alias le='ll --color=always|less -N'
