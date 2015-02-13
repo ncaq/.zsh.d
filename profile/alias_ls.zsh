3d3f56b8d00effc6c419b60b8edb4432ac1cb413
ncaq <ncaq@ncaq.net>
2015-02-13 17:36:54 +0900

    alias_ls.zsh: alias -g

--- profile/alias_ls.zsh
+++ profile/alias_ls.zsh
@@ -1,3 +1,3 @@
-alias ls='ls-first-ln-dir --color=auto --classify --human-readable --almost-all'
-alias ll='ls-first-ln-dir --color=auto --classify --human-readable --all -l'
-alias le='ll --color=always|less -N'
+alias -g ls='ls-first-ln-dir --color=auto --classify --human-readable --almost-all'
+alias -g ll='ls-first-ln-dir --color=auto --classify --human-readable --all -l'
+alias -g le='ll --color=always|less -N'
