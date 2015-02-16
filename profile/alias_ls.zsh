87b1d66ca75a274fa6673a1c611ed106fa07ac2f
ncaq <ncaq@ncaq.net>
2015-02-16 15:18:43 +0900

    alias_*: rename

--- profile/alias_ls.zsh
+++
@@ -1,3 +0,0 @@
-alias -g ls='ls-first-ln-dir --color=auto --classify --human-readable --almost-all'
-alias -g ll='ls-first-ln-dir --color=auto --classify --human-readable --all -l'
-alias -g le='ll --color=always|less -N'
