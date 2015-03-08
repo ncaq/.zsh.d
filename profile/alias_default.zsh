e64469543a8d987db36fa3b418f92f431a6b1f32
ncaq <ncaq@ncaq.net>
2015-03-09 01:09:06 +0900

    disabled parallel notice disabled emerge tree view

--- profile/alias_default.zsh
+++ profile/alias_default.zsh
@@ -1,6 +1,7 @@
 alias -g ag='ag -i'
 alias -g eix-sync='eix-sync -aM'
 alias -g make='make -j6'
+alias -g parallel='parallel --no-notice'
 alias -g tree="tree -C"
 alias eix-diff='eix-diff -F'
 alias eix='eix -F'
