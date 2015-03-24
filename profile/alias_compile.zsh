5aa53f52da717234a2ad9e0781ea6d05c1285357
ncaq <ncaq@ncaq.net>
2015-03-24 17:31:56 +0900

    moved: compile alias moved to alias_compile.zsh

---
+++ profile/alias_compile.zsh
@@ -0,0 +1,4 @@
+alias clang++y='clang++ -std=c++14 -stdlib=libc++ -ggdb -Wall -Wextra'
+alias g++x='g++ -std=c++11 -ggdb -Wall -Wextra'
+alias gcc1='gcc -std=c11 -ggdb -Wall -Wextra'
+alias hsr='ghc -O2 -threaded -Wall -outputdir /tmp'
