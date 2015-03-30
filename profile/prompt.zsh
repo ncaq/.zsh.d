0a2b6958cf6572f9ab28ade6df7640f463502c6d
ncaq <ncaq@ncaq.net>
2015-03-30 23:14:50 +0900

    modified: PROMPT fit to gnome-terminal theme

--- profile/prompt.zsh
+++ profile/prompt.zsh
@@ -1,4 +1,5 @@
-PROMPT="%K{magenta}%B%U%F{green}%n@%m%f %(?,%F{white},%F{red})%?%f %F{blue}%y%f %~%b%u%k%#"
+autoload -U colors && colors
+PROMPT=$'\e[48;5;230m\e[38;5;241m%B%D{%Y-%m-%dT%T} %n@%m/%y%(?,\e[38;5;240m,\e[38;5;160m)(%?)\e[38;5;241m\e[48;5;254m %~%# %b\e[m%'
 
 #ls when cd
 function chpwd() {ls}
