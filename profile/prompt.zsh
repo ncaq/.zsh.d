a85c8a22880355aade2644ca5caff824ee4a7f58
ncaq <ncaq@ncaq.net>
2015-03-31 00:44:54 +0900

    fixed: zsh ignore special char in PROMPT

--- profile/prompt.zsh
+++ profile/prompt.zsh
@@ -1,5 +1,5 @@
 autoload -U colors && colors
-PROMPT=$'\e[48;5;230m\e[38;5;241m%B%D{%Y-%m-%dT%T} %n@%m/%y%(?,\e[38;5;240m,\e[38;5;160m)(%?)\e[38;5;241m\e[48;5;254m %~%# %b\e[m%'
+PROMPT=$'%{\e[48;5;230m\e[38;5;241m%B%}%D{%Y-%m-%dT%T} %n@%m/%y%{%(?,\e[38;5;240m,\e[38;5;160m)%}(%?)%{\e[38;5;241m\e[48;5;254m%} %~%# %{%b\e[m%}'
 
 #ls when cd
 function chpwd() {ls}
