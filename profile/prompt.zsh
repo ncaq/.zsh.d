a0bbea0186deab6eadcf31f661995c76806fecac
ncaq <ncaq@ncaq.net>
2015-09-23 23:22:48 +0900

    added: profile/prompt: workaround to tramp timeout

--- profile/prompt.zsh
+++ profile/prompt.zsh
@@ -1,6 +1,9 @@
-autoload -U colors && colors
+[[ $TERM == "dumb" ]] && unsetopt zle && PS1='$ ' && return
+
 PROMPT=$'%{\e[48;5;230m\e[38;5;241m%B%}%D{%Y-%m-%dT%T} %n@%m/%y%{%(?,\e[38;5;240m,\e[38;5;160m)%}(%?)%{\e[38;5;241m\e[48;5;254m%} %~
 %# %{%b\e[m%}'
+
+autoload -U colors && colors
 
 #ls when cd
 function chpwd() {ls}
