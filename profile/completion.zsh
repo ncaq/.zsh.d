d33bf25af2dfa829f09c62b94de85ffce9301b86
ncaq <ncaq@ncaq.net>
2015-09-29 18:41:50 +0900

    cleaned: indent comment

--- profile/completion.zsh
+++ profile/completion.zsh
@@ -1,15 +1,14 @@
-#zsh-completions用の設定
 fpath=($ZDOTDIR/sub/zsh-completions/src $fpath)
 
-export LISTMAX=0 #0にすると､ウィンドウを超えて出力されるときにのみ問い合わせる http://d.hatena.ne.jp/tsaka/20060923/1158993348
+export LISTMAX=0                # 0にすると､ウィンドウを超えて出力されるときにのみ問い合わせる http://d.hatena.ne.jp/tsaka/20060923/1158993348
 
-setopt globdots #.から始まるファイルも展開する
-setopt list_packed #補完候補を詰めて表示する
-setopt list_types #補完候補の表示をls -Fのようにする
-setopt numericglobsort #数字補完リストを数値としてソートする
+setopt globdots                 # .から始まるファイルも展開する
+setopt list_packed              # 補完候補を詰めて表示する
+setopt list_types               # 補完候補の表示をls -Fのようにする
+setopt numericglobsort          # 数字補完リストを数値としてソートする
 
-#補完の情報を増やす
-#http://qiita.com/PSP_T/items/ed2d36698a5cc314557d
+# 補完の情報を増やす
+# http://qiita.com/PSP_T/items/ed2d36698a5cc314557d
 # highlight
 zstyle ':completion:*:default' menu select=2
 # 補完を分類する
@@ -20,10 +19,10 @@ zstyle ':completion:*' group-name ''
 # セパレータを設定する
 zstyle ':completion:*' list-separator '->'
 zstyle ':completion:*:manuals' separate-sections true
-
-zstyle ':completion:*:processes' command 'ps ax -o pid,s,args' #processIDを得るためのコマンドを指定
-
-zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}' #大文字小文字を区別しない
+# processIDを得るためのコマンドを指定
+zstyle ':completion:*:processes' command 'ps ax -o pid,s,args'
+# 大文字小文字を区別しない
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
 autoload -U compinit
 compinit -u -d /tmp/$USER.zcompdump
