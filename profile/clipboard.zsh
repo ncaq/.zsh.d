92e391df5a71444b65da17eec638ee5f2081260f
ncaq <ncaq@ncaq.net>
2015-09-29 18:38:19 +0900

    added: kill-region-clipboard

--- profile/clipboard.zsh
+++ profile/clipboard.zsh
@@ -1,3 +1,22 @@
+function copy-cutbuffer-to-clipboard ()
+{
+    echo -n $CUTBUFFER|xsel --clipboard --input --logfile /tmp/$USER.xsel.log
+}
+
+function copy-clipboard-to-cutbuffer ()
+{
+    CUTBUFFER=$(echo ""|xsel --clipboard --output --logfile /tmp/$USER.xsel.log)
+}
+
+function kill-region-clipboard ()
+{
+    zle kill-region
+    copy-cutbuffer-to-clipboard
+}
+
+zle -N kill-region-clipboard
+bindkey "^W" kill-region-clipboard
+
 function copy-region-as-kill-clipboard ()
 {
     zle copy-region-as-kill
@@ -10,7 +29,7 @@ bindkey "^[w" copy-region-as-kill-clipboard
 
 function yank-clipboard ()
 {
-    CUTBUFFER=$(echo ""|xsel --clipboard --output --logfile /tmp/$USER.xsel.log)
+    copy-clipboard-to-cutbuffer
     zle yank
 }
 
