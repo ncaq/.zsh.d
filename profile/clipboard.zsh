f502decc71c4e8948d188f1c19280abeca9de13e
ncaq <ncaq@ncaq.net>
2015-09-29 18:33:26 +0900

    fixed: clipboard: use --clipboard section

--- profile/clipboard.zsh
+++ profile/clipboard.zsh
@@ -1,15 +1,16 @@
 function copy-region-as-kill-clipboard ()
 {
     zle copy-region-as-kill
-    echo $CUTBUFFER|xsel --input --logfile /tmp/$USER.xsel.log
+    echo $CUTBUFFER|xsel --clipboard --input --logfile /tmp/$USER.xsel.log
 }
 
 zle -N copy-region-as-kill-clipboard
+bindkey "^[W" copy-region-as-kill-clipboard
 bindkey "^[w" copy-region-as-kill-clipboard
 
 function yank-clipboard ()
 {
-    CUTBUFFER=$(echo ""|xsel --output --logfile /tmp/$USER.xsel.log)
+    CUTBUFFER=$(echo ""|xsel --clipboard --output --logfile /tmp/$USER.xsel.log)
     zle yank
 }
 
