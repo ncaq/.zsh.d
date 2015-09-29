b1b91e2730b90f3a466ccede75a60d1232a3e2c3
ncaq <ncaq@ncaq.net>
2015-09-29 17:15:51 +0900

    added: setopt auto_cd

--- profile/load.zsh
+++ profile/load.zsh
@@ -1,14 +1,10 @@
-setopt autopushd
-setopt correct
-setopt extended_glob
+setopt auto_cd                  # cdの省略
+setopt autopushd                # ディレクトリを自動で記録
+setopt correct                  # 入力ミス自動修正
+setopt extended_glob            # 拡張glob
 
-if [ -f "$ZDOTDIR/sub/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" ];then
-    source $ZDOTDIR/sub/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-fi
-
-if [ -f '/etc/profile.d/autojump.zsh' ];then
-    source /etc/profile.d/autojump.zsh
-fi
+autoload -U locate-current-dir
+autoload -U locate-current-dir-null
 
 autoload -U history-search-end
 zle -N history-beginning-search-backward-end history-search-end
@@ -20,5 +16,10 @@ zle -N percol-history
 autoload -U percol-subfind
 zle -N percol-subfind
 
-autoload -U locate-current-dir
-autoload -U locate-current-dir-null
+if [ -f "$ZDOTDIR/sub/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" ];then
+    source $ZDOTDIR/sub/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+fi
+
+if [ -f '/etc/profile.d/autojump.zsh' ];then
+    source /etc/profile.d/autojump.zsh
+fi
