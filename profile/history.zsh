02054c6c635847fed0a82ae7836b55673f571370
ncaq <ncaq@ncaq.net>
2015-10-09 15:54:32 +0900

    added: profile/history: hist_reduce_blanks

--- profile/history.zsh
+++ profile/history.zsh
@@ -5,4 +5,5 @@ export SAVEHIST=1000000
 setopt extended_history         # 開始時刻と実行時間を記録する
 setopt hist_ignore_dups         # 連続する重複コマンドを記録しない
 setopt hist_ignore_space        # スペースで始めると記録しない
+setopt hist_reduce_blanks       # 複数のスペースを省略
 setopt share_history            # 複数端末で履歴を共有する
