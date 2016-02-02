039c35451490255bbdf9f28e3c19feb8b30f6538
ncaq <ncaq@ncaq.net>
2016-02-02 22:07:03 +0900

    fixed: need export

--- profile/value.zsh
+++ profile/value.zsh
@@ -1,5 +1,5 @@
-EDITOR='emacsclient -a emacs'
-LESS='--ignore-case --long-prompt --RAW-CONTROL-CHARS'
-LESSHISTFILE=/tmp/.lesshst
-PATH=$PATH:/sbin:/usr/sbin:/usr/games/bin:~/.cabal/bin
-VISUAL=$EDITOR
+export EDITOR='emacsclient -a emacs'
+export LESS='--ignore-case --long-prompt --RAW-CONTROL-CHARS'
+export LESSHISTFILE='-'
+export PATH=$PATH:/sbin:/usr/sbin:/usr/games/bin:~/.cabal/bin
+export VISUAL=$EDITOR
