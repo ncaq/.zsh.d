8393d3a9fd90a975c664c2db2aa46ab78d3a2dbc
ncaq <ncaq@ncaq.net>
2016-03-29 18:23:10 +0900

    changed: prefer user setting PATH

--- profile/value.zsh
+++ profile/value.zsh
@@ -1,5 +1,5 @@
 export EDITOR='emacsclient -a emacs'
 export LESS='--ignore-case --long-prompt --RAW-CONTROL-CHARS'
 export LESSHISTFILE='-'
-export PATH=$PATH:/sbin:/usr/sbin:/usr/games/bin:~/.local/bin
+export PATH=/sbin:/usr/sbin:/usr/games/bin:~/.local/bin:$PATH
 export VISUAL=$EDITOR
