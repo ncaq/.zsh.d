9a8662413f98497a8e769ec97b1bc7a165f50012
ncaq <ncaq@ncaq.net>
2017-12-19 15:25:22 +0900

    changed: makeのコア数を現在のCPUコア数に合わせる

--- profile/alias-default.zsh
+++ profile/alias-default.zsh
@@ -1,7 +1,7 @@
 alias eix-diff='eix-diff -F'
 alias eix-sync='eix-sync -aM'
 alias eix='eix -F'
-alias make='make -j5'
+alias make="make -j$(nproc)"
 alias parallel='parallel --no-notice'
 alias sudo='sudo '
 alias tree='tree -hlpCF --sort=version --dirsfirst'
