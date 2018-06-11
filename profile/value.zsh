66bf8d8c653878bb498c136cfe178966a1cc902a
ncaq <ncaq@ncaq.net>
2018-06-11 20:32:17 +0900

    added: TERM=gnome-256color

--- profile/value.zsh
+++ profile/value.zsh
@@ -2,4 +2,5 @@ export EDITOR='emacsclient -a emacs'
 export LESS='--ignore-case --long-prompt --RAW-CONTROL-CHARS'
 export LESSHISTFILE='-'
 export PATH=$PATH:/sbin:/usr/sbin:/usr/games/bin:~/.local/bin:~/.config/yarn/global/node_modules/.bin
+export TERM=gnome-256color
 export VISUAL=$EDITOR
