e4b917fb73648db766639b3334179e97aa91a488
ncaq <ncaq@ncaq.net>
2018-11-02 17:54:22 +0900

    added: path: rustup

--- profile/value.zsh
+++ profile/value.zsh
@@ -1,6 +1,6 @@
 export EDITOR='emacsclient -a emacs'
 export LESS='--ignore-case --long-prompt --RAW-CONTROL-CHARS'
 export LESSHISTFILE='-'
-export PATH=$PATH:/sbin:/usr/sbin:/usr/games/bin:~/.local/bin:~/.config/yarn/global/node_modules/.bin
+export PATH=$PATH:/sbin:/usr/sbin:/usr/games/bin:~/.local/bin:~/.config/yarn/global/node_modules/.bin:~/.cargo/bin
 export TERM=gnome-256color
 export VISUAL=$EDITOR
