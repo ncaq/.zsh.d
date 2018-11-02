8af62feaf07c220a6613190caefc85a6448e7999
ncaq <ncaq@ncaq.net>
2018-11-02 20:23:09 +0900

    added: path: go

--- profile/value.zsh
+++ profile/value.zsh
@@ -1,6 +1,7 @@
 export EDITOR='emacsclient -a emacs'
+export GOPATH=~/.go
 export LESS='--ignore-case --long-prompt --RAW-CONTROL-CHARS'
 export LESSHISTFILE='-'
-export PATH=$PATH:/sbin:/usr/sbin:/usr/games/bin:~/.local/bin:~/.config/yarn/global/node_modules/.bin:~/.cargo/bin
+export PATH=$PATH:/sbin:/usr/sbin:/usr/games/bin:~/.local/bin:~/.config/yarn/global/node_modules/.bin:~/.cargo/bin:$GOPATH/bin
 export TERM=gnome-256color
 export VISUAL=$EDITOR
