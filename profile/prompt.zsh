c6e701538127dbfd49cef5757a5cda76693cf527
ncaq <ncaq@ncaq.net>
2020-01-08 16:23:48 +0900

    added: starship setting

--- profile/prompt.zsh
+++ profile/prompt.zsh
@@ -1,10 +1,15 @@
 [[ $TERM == 'dumb' ]] && unsetopt zle && PS1='$ ' && return
 
-PROMPT=$'%{\e[48;5;230m\e[38;5;241m%B%}%D{%Y-%m-%dT%T} %n@%m/%y%{%(?,\e[38;5;240m,\e[38;5;160m)%}(%?)%{\e[38;5;241m\e[48;5;254m%} %~\n%# %{%b\e[m%}'
-
 autoload -U colors && colors
 
-#ls when cd
+# ls when cd
 chpwd() {
     ls
 }
+
+if hash starship 2>/dev/null; then
+    eval "$(starship init zsh)"
+    return
+fi
+
+PROMPT=$'%{\e[48;5;230m\e[38;5;241m%B%}%D{%Y-%m-%dT%T} %n@%m/%y%{%(?,\e[38;5;240m,\e[38;5;160m)%}(%?)%{\e[38;5;241m\e[48;5;254m%} %~\n%# %{%b\e[m%}'
