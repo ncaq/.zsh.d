3d1c8934622c6c2d969a81a38f7788e75ddb45de
ncaq <ncaq@ncaq.net>
2020-07-01 14:38:02 +0900

    deleted: git -> hub
    hub環境を整える必要のない使い捨ての場所で一々コメントアウトするのが面倒になったため.
    hubコマンド自体はhubをそのまま使えば良いので問題にならない.

--- profile/alias-default.zsh
+++ profile/alias-default.zsh
@@ -2,7 +2,6 @@ alias cp='cp --reflink=auto -p --verbose'
 alias eix-diff='eix-diff -F'
 alias eix-sync='sudo eix-sync -aM'
 alias eix='eix -F'
-alias git='hub'
 alias make="make -j$(nproc)"
 alias parallel='parallel --no-notice'
 alias sudo='sudo '
