f570e7fec9713a5d0d17dde69727e9291161e79e
ncaq <ncaq@ncaq.net>
2021-06-30 17:35:04 +0900

    added: mv to verbose

--- profile/alias-default.zsh
+++ profile/alias-default.zsh
@@ -3,6 +3,7 @@ alias eix-diff='eix-diff -F'
 alias eix-sync='sudo eix-sync -aM'
 alias eix='eix -F'
 alias make="make -j$(nproc)"
+alias mv='mv --interactive --verbose'
 alias parallel='parallel --no-notice'
 alias sudo='sudo '
 alias tree='tree -hlpCF --sort=version --dirsfirst'
