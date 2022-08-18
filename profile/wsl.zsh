10c9035cac2656fb58476d4ebb3573639a4f0ad8
ncaq <ncaq@ncaq.net>
2022-08-18 14:36:34 +0900

    feat: 現在のディレクトリの複製

---
+++ profile/wsl.zsh
@@ -0,0 +1,7 @@
+if hash wslpath 2>/dev/null; then
+  # [同じディレクトリ内のタブまたはペインを開く | Microsoft Docs](https://docs.microsoft.com/ja-jp/windows/terminal/tutorials/new-tab-same-directory)
+  keep_current_path() {
+    printf "\e]9;9;%s\e\\" "$(wslpath -w "$PWD")"
+  }
+  precmd_functions+=(keep_current_path)
+fi
