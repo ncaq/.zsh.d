1773b385ef88510611572114243c0267502efa25
ncaq <ncaq@ncaq.net>
2023-03-03 08:19:23 +0900

    feat: github-issue-markdown
    [アカウントごとのGitHub Projectsを使った公開TODO管理が割と良い感じ · Issue · ncaq/www.ncaq.net](https://github.com/ncaq/www.ncaq.net/issues/145)

--- profile/alias-function.zsh
+++ profile/alias-function.zsh
@@ -25,6 +25,7 @@ alias genkernel-update='sudo genkernel --no-lvm --btrfs --luks initramfs && grub
 alias git-daily='git log --all --format="%h %ai %s" --since=$(date +"%Y-%m-%d-00:00:00") --author=$(git config user.email)|xsel --clipboard --input --logfile /dev/null'
 alias git-locate-pull='locate --regex "$(pwd)/.*/\.git$" --null|parallel --null --keep-order "test -d {} -a -d {}/.. && cd {}/.. && echo {}: && git -c color.diff=always pull --progress --all --keep"'
 alias git-submodule-pull='git submodule foreach "git checkout master && git pull"'
+alias github-issue-markdown='gh issue view --comments --json body --jq ".body" --json comments --jq ".body, .comments.[].body"'
 alias github-label-setup-dotfiles='github-label-setup --labels ~/dotfiles/github/github-label-presets.json'
 alias grub-update='sudo grub-mkconfig -o /boot/grub/grub.cfg'
 alias iconv-cp932='iconv --from-code CP932 --to-code UTF-8'
