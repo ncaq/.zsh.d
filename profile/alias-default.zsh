9e325da96038df1a6e7529a4d54e1d6762c138a2
ncaq <ncaq@ncaq.net>
2026-01-04 16:35:05 +0900

    feat(profile)!: remove Gentoo-specific aliases from zsh configs

--- profile/alias-default.zsh
+++ profile/alias-default.zsh
@@ -1,7 +1,4 @@
 alias cp='cp --reflink=auto -p --verbose'
-alias eix-diff='eix-diff -F'
-alias eix-sync='sudo eix-sync -aM'
-alias eix='eix -F'
 alias make="make -j$(nproc)"
 alias mv='mv --interactive --verbose'
 alias parallel='parallel --no-notice'
