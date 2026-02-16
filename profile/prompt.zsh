fa6723b7021906e9234758c464ec24b7a28fec24
ncaq <ncaq@ncaq.net>
2026-02-16 16:19:23 +0900

    feat: add modus-vivendi color theme
    - Introduce color.zsh with modus-vivendi palette for LS_COLORS and
      fast-syntax-highlighting
    - Update prompt.zsh to use modus-vivendi colors for custom prompt when
      starship is unavailable

--- profile/prompt.zsh
+++ profile/prompt.zsh
@@ -11,6 +11,6 @@ if hash starship 2>/dev/null; then
   return
 fi
 
-# starshipが使えない場合自前のプロンプトを設定する
+# starshipが使えない場合自前のプロンプトを設定する (modus-vivendi)
 autoload -Uz colors && colors
-PROMPT=$'%{\e[48;5;230m\e[38;5;241m%B%}%D{%Y-%m-%dT%T} %n@%m/%y%{%(?,\e[38;5;240m,\e[38;5;160m)%}(%?)%{\e[38;5;241m\e[48;5;254m%} %~\n%# %{%b\e[m%}'
+PROMPT=$'%{\e[48;2;30;30;30m\e[38;2;255;255;255m%B%}%D{%Y-%m-%dT%T} %n@%m/%y%{%(?,\e[38;2;152;152;152m,\e[38;2;255;95;89m)%}(%?)%{\e[38;2;198;218;255m\e[48;2;0;0;0m%} %~\n%# %{%b\e[m%}'
