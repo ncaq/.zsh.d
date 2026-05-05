4d9e65669d03ef9906e7fc96e34bc43442e36fe2
ncaq <ncaq@ncaq.net>
2026-05-05 21:19:22 +0900

    feat: avif向けのエイリアスを追加
    大多数の場合に対応出来るような雑なエイリアスを追加しました。
    真面目にやるならtuneとか考えてエンコードしますが、
    大抵の場合はこれで十分でしょう。

--- profile/codec.zsh
+++ profile/codec.zsh
@@ -1,3 +1,8 @@
+alias avifenc-base='avifenc --jobs all --speed 0'
+alias avifenc-lossless='avifenc-basic --lossless'
+alias avifenc-lossy-base='avifenc-base --advanced aq-mode=1 --advanced enable-chroma-deltaq=1 --advanced enable-qm=1 --advanced qm-min=0 --advanced qm-max=15 --advanced deltaq-mode=3 --advanced end-usage=q'
+alias avifenc-lossy-good='avifenc-lossy-base --advanced cq-level=18'
+alias avifenc-lossy-lite='avifenc-lossy-base --advanced cq-level=32'
 alias cwebp-lossy='cwebp -m 6 -mt -af -metadata all'
 alias opusenc-speech='opusenc --speech --framesize 60'
 alias oxipng-best='oxipng --opt max --strip safe --interlace 0 --zopfli'
