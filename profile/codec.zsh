f88a3708a00f874f7786e285f5859f8c683a7832
ncaq <ncaq@ncaq.net>
2026-05-05 21:15:30 +0900

    feat: parallelによるラップを除去
    parallelを組み合わせるのは呼び出し側の責務であるべきだから。

--- profile/codec.zsh
+++ profile/codec.zsh
@@ -1,3 +1,3 @@
-alias cwebp-lossy='parallel "cwebp -m 6 -mt -af -metadata all {} -o {.}.webp" :::'
-alias opusenc-speech='parallel "opusenc --speech --framesize 60 {} {.}.opus" :::'
-alias oxipng-best='parallel "oxipng --opt max --strip safe --interlace 0 --zopfli" :::'
+alias cwebp-lossy='cwebp -m 6 -mt -af -metadata all'
+alias opusenc-speech='opusenc --speech --framesize 60'
+alias oxipng-best='oxipng --opt max --strip safe --interlace 0 --zopfli'
