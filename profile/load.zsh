f46ce5fe27a501e8d2059cb899b623501433af5f
ncaq <ncaq@ncaq.net>
2026-07-14 11:44:21 +0900

    fix: `extended_glob`を有効にしない
    機能を使っていないし、
    nixの構文と衝突するため。

--- profile/load.zsh
+++ profile/load.zsh
@@ -1,6 +1,5 @@
 setopt auto_cd                  # cdの省略
 setopt autopushd                # ディレクトリを自動で記録
-setopt extended_glob            # 拡張glob
 
 # ユーザディレクトリにある関数を全てautoloadする
 for i in "$ZDOTDIR"/.zsh.d/autoload/*
